f3ff23cd275552904be0e6eeed3ce5f5c49df43f
tehasome <te_hasome@hotmail.com>
2022-10-09 14:07:36 +0700

    Install project

---
+++ constant.go
@@ -0,0 +1,10 @@
+package constrant
+
+type TestConstrant struct {
+	Test string
+	Pass string
+}
+
+func TestPrintConstrant() string {
+	return "Hello"
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tehasome/constrant
+
+go 1.19
